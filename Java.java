9ff958cf2afafb7d4d3df8cfd2e7da8fa4a24df3
rakeshbontala <rakeshbonthala1998@gmail.com>
2024-01-03 22:37:32 +0530

    initial commit

---
+++ Java.java
@@ -0,0 +1,6 @@
+class Helloworld{
+    public static void main(String[] args) throws
+    Exception {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Python.py
@@ -0,0 +1,4 @@
+a='100'
+
+for i in (a):
+    print(i)

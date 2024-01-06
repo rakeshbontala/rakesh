49df83ec2db9e9c72b86209ad6382a12f9e0a9a2
rakeshbontala <rakeshbonthala1998@gmail.com>
2024-01-07 01:42:53 +0530

    #

---
+++ Python/CODING PRACTICE -1A/Hello World.py
@@ -0,0 +1,9 @@
+#Write a program that takes a word W as input and prints 'Hello' followed by the given word W
+
+Word = input()
+
+add_Hello = 'Hello '
+
+print(add_Hello+Word)
+
+
---
+++ Python/CODING PRACTICE -1A/Print In Reverse Oder.py
@@ -0,0 +1,7 @@
+#Print the message given in the second line of input before the first line of input 
+
+first_line = input()
+second_line = input()
+
+print(second_line)
+print(first_line)

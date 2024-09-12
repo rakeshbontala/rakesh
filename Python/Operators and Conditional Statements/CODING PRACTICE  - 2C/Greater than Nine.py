8feadeacba968c1ed285ef4fc641d65d14b2dbec
rakeshbontala <rakeshbonthala1998@gmail.com>
2024-09-12 23:54:32 +0530

    #

---
+++ Python/Operators and Conditional Statements/CODING PRACTICE  - 2C/Greater than Nine.py
@@ -0,0 +1,3 @@
+print(int(input())>9 and int(input()) >9 )
+
+#a> and b>9 then true otherwise false
---
+++ Python/Operators and Conditional Statements/CODING PRACTICE  - 2C/Negative Numbers.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+
+if a<0 and b<0:
+    print(True)
+else:
+    print(False)
+    
+#a<0 and b<0 then true otherwise false
